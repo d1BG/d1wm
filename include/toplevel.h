2dd89c6d1b91a9be8de71418c40160582db25d7d
d1bg <deni111bg@gmail.com>
2025-02-24 17:39:16 +0200

    chore: whoops

--- include/toplevel.h
+++ include/toplevel.h
@@ -22,3 +22,8 @@ struct d1_toplevel {
 void xdg_toplevel_commit(struct wl_listener *listener, void *data);
 void xdg_toplevel_unmap(struct wl_listener *listener, void *data);
 void xdg_toplevel_map(struct wl_listener *listener, void *data);
+
+void xdg_toplevel_request_move(struct wl_listener *listener, void *data);
+void xdg_toplevel_request_resize(struct wl_listener *listener, void *data);
+void xdg_toplevel_request_maximize(struct wl_listener *listener, void *data);
+void xdg_toplevel_request_fullscreen(struct wl_listener *listener, void *data);
